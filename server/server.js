744042d93f2177a9547a469fcc0d731adb78e505
Jayashree T <jayashree@example.com>
2026-03-20 00:57:40 +0530

    Serve React frontend via Express

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ mongoose.connect('mongodb://localhost:27017/order_insight_dashboard', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 // Routes
 const orderRoutes = require('./routes/orders');
@@ -38,4 +38,14 @@ app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+const path = require('path');
+
+// Serve React build folder
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Return index.html for all other routes (React routing)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
